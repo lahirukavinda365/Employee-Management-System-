8a4bbb3b323dcd06d980f7be3e916b77704c5435
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2024-01-07 17:52:15 +0530

    save employee service layer part completed

--- src/main/java/com/example/employeeManagementSystem/service/EmployeeService.java
+++ src/main/java/com/example/employeeManagementSystem/service/EmployeeService.java
@@ -1,5 +1,29 @@
 package com.example.employeeManagementSystem.service;
 
+import com.example.employeeManagementSystem.dto.EmployeeDTO;
+import com.example.employeeManagementSystem.entity.Employee;
+import com.example.employeeManagementSystem.repo.EmployeeRepo;
+import com.example.employeeManagementSystem.util.VarList;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class EmployeeService {
 
+    @Autowired
+    private EmployeeRepo employeeRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public String saveEmployee(EmployeeDTO employeeDTO){
+        if (employeeRepo.existsById(employeeDTO.getEmpID())){
+                return VarList.RSP_DUPLICATED;
+        }else {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+        }
+    }
 }
---
+++ src/main/java/com/example/employeeManagementSystem/util/VarList.java
@@ -0,0 +1,13 @@
+package com.example.employeeManagementSystem.util;
+
+public class VarList {
+    public static final String RSP_SUCCESS = "80";
+    public static final String RSP_NO_DATA_FOUND = "81";
+    public static final String RSP_NOT_AUTHORIZED = "82";
+    public static final String RSP_TOKEN_EXPIRED = "83";
+    public static final String RSP_TOKEN_INVALID = "84";
+    public static final String RSP_ERROR = "85";
+    public static final String RSP_DUPLICATED = "86";
+    public static final String RSP_FAIL = "86";
+
+}
