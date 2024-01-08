cfc2f494e327aa0ebe934c68dc86ca0fd3153333
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2024-01-08 13:40:14 +0530

    get all employee details function completed

--- src/main/java/com/example/employeeManagementSystem/controller/EmployeeController.java
+++ src/main/java/com/example/employeeManagementSystem/controller/EmployeeController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/employees")
 public class EmployeeController {
@@ -49,5 +51,58 @@ public class EmployeeController {
     }
 
 
+
+        }
+
+
+
+    @PutMapping(value = "/updateEmp")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        try{
+            String res = employeeService.updateEmployee(employeeDTO);
+            if (res.equals("80")){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("SUCCESS");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            } else if (res.equals("86")) {
+                responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No Registered User");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }else {
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("FAILED");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage("SERVER ERROR");
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+        }
+        }
+
+    @GetMapping(value = "/getAllEmp")
+    public ResponseEntity gatAllEmployee() {
+        try{
+            List<EmployeeDTO> employeeDTOList = employeeService.getAllEmployee();
+
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("SUCCESS");
+            responseDTO.setContent(employeeDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+        }catch (Exception e) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage("SERVER ERROR");
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
     }
+
+
 }
--- src/main/java/com/example/employeeManagementSystem/service/EmployeeService.java
+++ src/main/java/com/example/employeeManagementSystem/service/EmployeeService.java
@@ -5,9 +5,13 @@ import com.example.employeeManagementSystem.entity.Employee;
 import com.example.employeeManagementSystem.repo.EmployeeRepo;
 import com.example.employeeManagementSystem.util.VarList;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -26,4 +30,23 @@ public class EmployeeService {
             return VarList.RSP_SUCCESS;
         }
     }
-}
+
+    public String updateEmployee(EmployeeDTO employeeDTO){
+
+        if (employeeRepo.existsById(employeeDTO.getEmpID())){
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+
+    }
+
+    public List<EmployeeDTO> getAllEmployee(){
+         List<Employee> employeeList =  employeeRepo.findAll();
+         return modelMapper.map(employeeList,new TypeToken<ArrayList<EmployeeDTO>>(){}.getType());
+
+        }
+    }
+
